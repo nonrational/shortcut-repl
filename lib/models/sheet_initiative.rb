9801f03cf114d5d2309ee4f9df22e19c5b058eb3
Alan Norton <git@nonration.al>
2023-09-27 22:24:31 -0400

    leave myself some notes

--- lib/models/sheet_initiative.rb
+++ lib/models/sheet_initiative.rb
@@ -3,41 +3,10 @@ class SheetInitiative
   include ActiveModel::Model
   attr_accessor :row_data, :row_index, :spreadsheet_id, :spreadsheet_range, :sheet_name
 
-  def row
-    @row ||= SheetRow.new(row_data: row_data, row_index: row_index, sheet_name: sheet_name)
-  end
-
-  def story?
-    /story-/.match?(row.shortcut_id)
-  end
-
-  def epic?
-    /epic-/.match?(row.shortcut_id)
-  end
-
-  def epic_id
-    @epic_id ||= row.shortcut_id.split("-")[1].to_i if epic?
-  end
-
-  def epic
-    @epic ||= Scrb.current_epics.find { |e| e.id == epic_id } if epic?
-  end
-
-  def move_document_to_correct_drive_location
-    # We tend to write documents and then share them with the right audience.
-  end
-
-  def sheets_v4
-    @sheets_v4 ||= Google::Apis::SheetsV4::SheetsService.new
-  end
-
-  def auth_client
-    @auth_client ||= GoogleCredentials.load!
-  end
-
   def pull
     pull_name_from_epic
-    pull_dates_and_status_from_epic
+    pull_target_dates_from_epic
+    pull_status_from_epic
     pull_story_stats_from_epic
   end
 
@@ -45,9 +14,16 @@ class SheetInitiative
     push_dates_and_status_to_epic
   end
 
-  def pull_dates_and_status_from_epic
-    true
+  def move_document_to_correct_drive_location
+    # TODO: We tend to write documents and then share them with the right audience.
+    #       This method should eventually move an individual PAD/RFC to the correct folder.
   end
+
+  #               _      _                  _
+  #  _ __ _  _ __| |_   | |_ ___   ___ _ __(_)__
+  # | '_ \ || (_-< ' \  |  _/ _ \ / -_) '_ \ / _|
+  # | .__/\_,_/__/_||_|  \__\___/ \___| .__/_\__|
+  # |_|                               |_|
 
   def push_dates_and_status_to_epic
     epic_workflow_state = EpicWorkflow.fetch.find_state_by_name(row.status)
@@ -61,6 +37,20 @@ class SheetInitiative
     result = epic.update(attrs)
     binding.pry unless result.success?
     @epic = Epic.new(result)
+  end
+
+  #            _ _    __                          _
+  #  _ __ _  _| | |  / _|_ _ ___ _ __    ___ _ __(_)__
+  # | '_ \ || | | | |  _| '_/ _ \ '  \  / -_) '_ \ / _|
+  # | .__/\_,_|_|_| |_| |_| \___/_|_|_| \___| .__/_\__|
+  # |_|                                     |_|
+
+  def pull_target_dates_from_epic
+    # TODO
+  end
+
+  def pull_status_from_epic
+    # TODO
   end
 
   def pull_name_from_epic
@@ -93,6 +83,40 @@ class SheetInitiative
       value_input_option: "USER_ENTERED",
       options: {authorization: auth_client}
     )
+  end
+
+  #       _ _   _   _                     _
+  #  __ _| | | | |_| |_  ___   _ _ ___ __| |_
+  # / _` | | | |  _| ' \/ -_) | '_/ -_|_-<  _|
+  # \__,_|_|_|  \__|_||_\___| |_| \___/__/\__|
+  #
+
+  def row
+    @row ||= SheetRow.new(row_data: row_data, row_index: row_index, sheet_name: sheet_name)
+  end
+
+  def story?
+    /story-/.match?(row.shortcut_id)
+  end
+
+  def epic?
+    /epic-/.match?(row.shortcut_id)
+  end
+
+  def epic_id
+    @epic_id ||= row.shortcut_id.split("-")[1].to_i if epic?
+  end
+
+  def epic
+    @epic ||= Scrb.current_epics.find { |e| e.id == epic_id } if epic?
+  end
+
+  def sheets_v4
+    @sheets_v4 ||= Google::Apis::SheetsV4::SheetsService.new
+  end
+
+  def auth_client
+    @auth_client ||= GoogleCredentials.load!
   end
 
   def any_mismatch?
